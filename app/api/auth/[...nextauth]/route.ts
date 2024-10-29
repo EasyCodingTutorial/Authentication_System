ce0c0cbf6891510c73444b8682b48a0941f07219
Easy Coding tutorial <ecoding45@gmail.com>
2024-10-29 17:20:15 +0530

    next

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,9 @@ import userSchema from "@/Schema/userSchema";
 // For Bcryptjs
 import bcrypt from 'bcryptjs'
 
-import NextAuth, { NextAuthOptions } from "next-auth";
+import type { NextAuthOptions } from 'next-auth'
+
+import NextAuth from "next-auth";
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const authOptions: NextAuthOptions = {
