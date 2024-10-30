13836a48d8b9911d14bbaf8e9ec6a30e10a972c1
Easy Coding tutorial <ecoding45@gmail.com>
2024-10-30 11:37:32 +0530

    first

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,58 +1,6 @@
-// For Connection
-import ConnectToDb from "@/utils/connectToDb";
-
-// For Schema
-import userSchema from "@/Schema/userSchema";
-
-// For Bcryptjs
-import bcrypt from 'bcryptjs'
-
-import type { NextAuthOptions } from 'next-auth'
-
 import NextAuth from "next-auth";
-import CredentialsProvider from 'next-auth/providers/credentials';
 
-export const authOptions: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            credentials: {},
-            async authorize(credentials) {
-                const { email, password } = credentials as { email: string, password: string };
-
-                try {
-                    await ConnectToDb();
-                    const userEmailCheck = await userSchema.findOne({ email });
-
-                    if (!userEmailCheck) {
-                        return null;
-                    }
-
-                    const isPasswordCorrect = await bcrypt.compare(password, userEmailCheck.password);
-                    if (!isPasswordCorrect) {
-                        return null;
-                    }
-
-                    return {
-                        id: userEmailCheck._id,
-                        email: userEmailCheck.email,
-                        name: userEmailCheck.name
-                    };
-                } catch (error) {
-                    console.log("Auth Error", error);
-                    return null;
-                }
-            }
-        })
-    ],
-    session: {
-        strategy: "jwt"
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    pages: {
-        signIn: "/",
-        signOut: "/"
-    }
-};
+import { authOptions } from "@/utils/authOptions";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
